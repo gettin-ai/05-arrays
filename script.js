be4607c0f8f22558d7d0fe4d972d676720e1cb80
Christopher James Lind <clind@cpp.edu>
2026-03-17 00:45:55 +0000

    Refactor movie genre selection logic and improve movie list display

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 // Set up event listener for when the user selects a genre
+// Create an array for each movie genre
+const comedyMovies = ["Superbad", "Mean Girls", "Elf", "The Mask"];
+const actionMovies = ["Mad Max: Fury Road", "John Wick", "Black Panther", "Die Hard"];
+const dramaMovies = ["The Pursuit of Happyness", "Forrest Gump", "The Shawshank Redemption", "Titanic"];
+const sciFiMovies = ["Interstellar", "Star Wars", "The Matrix", "Avatar"];
+
 document.getElementById("genre").addEventListener("change", function() {
   // Get the selected genre
   const selectedGenre = document.getElementById("genre").value;
 
+  // Create a movieList array
+  let movieList = [];
+
   // Add movies to the movieList based on the selected genre
   if (selectedGenre === "Comedy") {
-    console.log("You selected 'Comedy'");
+    movieList = comedyMovies;
   } else if (selectedGenre === "Action") {
-    console.log("You selected 'Action'");
+    movieList = actionMovies;
   } else if (selectedGenre === "Drama") {
-    console.log("You selected 'Drama'");
+    movieList = dramaMovies;
   } else if (selectedGenre === "Sci-Fi") {
-    console.log("You selected 'Sci-Fi'");
+    movieList = sciFiMovies;
   }
 
   // Display the list of movies on the page
-  document.getElementById("movieRecommendations").innerText = `Enjoy: !`;
+  document.getElementById("movieRecommendations").innerText = `Enjoy: ${movieList.join(", ")}!`;
 });
